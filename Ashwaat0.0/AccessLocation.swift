c3ff497c27cfe951730748b8ae0a231a56a9ea35
maryambsq <maryamx399@gmail.com>
2025-04-29 21:40:19 +0300

    started working on AccessLocation interface

---
+++ Ashwaat0.0/AccessLocation.swift
@@ -0,0 +1,18 @@
+//
+//  AccessLocation.swift
+//  Ashwaat0.0
+//
+//  Created by Maryam Amer Bin Siddique on 01/11/1446 AH.
+//
+
+import SwiftUI
+
+struct AccessLocation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessLocation()
+}
