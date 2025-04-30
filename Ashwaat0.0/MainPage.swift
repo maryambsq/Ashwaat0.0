27b8594ab1167fc878666ce6995f646de934b02b
maryambsq <maryamx399@gmail.com>
2025-04-30 09:36:24 +0300

    wrapped up Access Location

---
+++ Ashwaat0.0/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  Ashwaat0.0
+//
+//  Created by Maryam Amer Bin Siddique on 02/11/1446 AH.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
